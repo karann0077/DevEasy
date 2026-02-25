4a0de585a9e6b9b2cf05a8338bf621886b1a4f2e
karann0077 <rajkaran3765@gmail.com>
2026-02-26 01:33:12 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,12 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    const backendUrl =
-      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
     return [
       {
         source: "/api/:path*",
-        destination: `${backendUrl}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/:path*`,
       },
     ];
   },
